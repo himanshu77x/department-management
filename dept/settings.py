b8a7c678950fd5b611dfab82a185779553233a45
Himanshu77x <himanshu77b@gmail.com>
2025-03-18 10:31:21 +0530

    Added Procfile and requirements.txt

--- dept/settings.py
+++ dept/settings.py
@@ -73,11 +73,14 @@ WSGI_APPLICATION = 'dept.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+
     }
 }
 
@@ -122,3 +125,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+import dj_database_url
+import environ
+import os
+
+env = environ.Env()
+environ.Env.read_env()
